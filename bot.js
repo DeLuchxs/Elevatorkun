f00fa3e6943c46f1b21d89d59806f0e89e32333b
DeLuchxs <liberi1418@gmail.com>
2023-06-18 14:57:59 +0200

    implemented eventhandler

--- bot.js
+++ bot.js
@@ -4,10 +4,6 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const { token } = require('./config.json');
 
-client.once(Events.ClientReady, c => {
-    console.log('Ready!');
-});
-
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -21,31 +17,23 @@ for (const folder of commandFolders) {
         if('data' in command && 'execute' in command) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log("[WARNING] The command at ${FilePath} does not have a 'data' or 'execute' property.");
+            console.log(`[WARNING] The command at "` + filePath + `" does not have a 'data' or 'execute' property.`);
         }
     }
 }
 
 
-client.on(Events.InteractionCreate, async interaction => {
-    if(!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    const command = interaction.client.commands.get(interaction.commandName);
-    if(!command) {
-        console.error('No mathching Command ${interaction.commandName} found.');
-        return;
-    }
-
-    try{
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if(interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(token);
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -7,7 +7,7 @@ module.exports = {
         .addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
 	async execute(interaction) {
 		const user = interaction.options.getUser('target');
-		if (user) return interaction.reply(`${user.username} joined the server on ${member.joinAt}`);
-		return interaction.reply(`${interaction.user.username} joined the server on ${interaction.member.joinAt}`);
+		if (user) return interaction.reply(`${user.username} joined the server on ${user.joinAt}`);
+		return interaction.reply(`${interaction.user.username} joined the server on ${interaction.users.joinAt}`);
     },
 };
