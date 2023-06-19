f4f5ab58e551975b3c86f4577e3c0f5e113003d4
DeLuchxs <liberi1418@gmail.com>
2023-06-19 17:55:24 +0200

    still not functional

--- commands/fun/join.js
+++ commands/fun/join.js
@@ -1,25 +1,34 @@
-// const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 
-// const { createAudioPlayer } = require('@discordjs/voice');
-// const player = createAudioPlayer();
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
+const path = require('path');
 
-// module.exports = {
-//     data : new SlashCommandBuilder()
-//     .setName('join')
-//     .setDescription('Join the voice channel you are in.'),
-//     async execute(interaction) {
-//         if(!channel) return interaction.reply('You need to be in a voice channel to use this command.');
-//         try {
-//             await connection.joinVoiceChannel();
-//             while (connection) {
-//                 connection.subscribe(player); 
-//                 player.play('C:\stuff\discord bot\audio\elevator.mp3');
-//             }
-//             player.stop();
-//             return connection;
-//         } catch (error) {
-//             connection.destroy();
-//             return interaction.reply('There was an error connecting to the voice channel.');
-//         }
-//     }
-// }
+module.exports = {
+    data : new SlashCommandBuilder()
+    .setName('join')
+    .setDescription('Join the voice channel you are in.'),
+    async execute(interaction) {
+        const voiceChannel = interaction.member.voice.channel;
+        if(!voiceChannel) return interaction.reply('You need to be in a voice channel to use this command.');
+        try {
+            const connection = joinVoiceChannel({
+                channelId: voiceChannel.id,
+                guildId: voiceChannel.guild.id,
+                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+            });
+            const player = createAudioPlayer();
+            const resource = createAudioResource(path.join(__dirname, 'audio', 'elevator.mp3'), {
+                inlineVolume: true
+            });
+            player.on('idle', () => {
+                player.play();
+            });
+            connection.subscribe(player);
+            player.play(resource);
+            return interaction.reply('Joined the voice channel.');
+        } catch (error) {
+            connection.destroy();
+            return interaction.reply('There was an error connecting to the voice channel.');
+        }
+    }
+}
