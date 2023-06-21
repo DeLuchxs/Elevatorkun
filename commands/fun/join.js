0169667960d4b84187020afa17dde4e2a7f1f872
DeLuchxs <liberi1418@gmail.com>
2023-06-21 22:25:47 +0200

    still figuring out join command

--- commands/fun/join.js
+++ commands/fun/join.js
@@ -1,34 +1,34 @@
-const { SlashCommandBuilder } = require('discord.js');
+// const { SlashCommandBuilder } = require('discord.js');
 
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
-const path = require('path');
+// const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+// const path = require('node:path');
 
-module.exports = {
-    data : new SlashCommandBuilder()
-    .setName('join')
-    .setDescription('Join the voice channel you are in.'),
-    async execute(interaction) {
-        const voiceChannel = interaction.member.voice.channel;
-        if(!voiceChannel) return interaction.reply('You need to be in a voice channel to use this command.');
-        try {
-            const connection = joinVoiceChannel({
-                channelId: voiceChannel.id,
-                guildId: voiceChannel.guild.id,
-                adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-            });
-            const player = createAudioPlayer();
-            const resource = createAudioResource(path.join(__dirname, 'audio', 'elevator.mp3'), {
-                inlineVolume: true
-            });
-            player.on('idle', () => {
-                player.play();
-            });
-            connection.subscribe(player);
-            player.play(resource);
-            return interaction.reply('Joined the voice channel.');
-        } catch (error) {
-            connection.destroy();
-            return interaction.reply('There was an error connecting to the voice channel.');
-        }
-    }
-}
+// module.exports = {
+//     data : new SlashCommandBuilder()
+//     .setName('join')
+//     .setDescription('Join the voice channel you are in.'),
+//     async execute(interaction) {
+//         const voiceChannel = interaction.member.voice.channel;
+//         if(!voiceChannel) return interaction.reply('You need to be in a voice channel to use this command.');
+//         try {
+//             const connection = joinVoiceChannel({
+//                 channelId: interaction.member.voice.channel.id,
+//                 guildId: interaction.guild.id,
+//                 adapterCreator: interaction.guild.voiceAdapterCreator,
+//             });
+//             const player = createAudioPlayer();
+//             const resource = createAudioResource(path.join(__dirname, 'audio', 'elevator.mp3'), {
+//                 inlineVolume: true
+//             });
+//             player.on('idle', () => {
+//                 player.play();
+//             });
+//             connection.subscribe(player);
+//             player.play(resource);
+//             return interaction.reply('Joined the voice channel.');
+//         } catch (error) {
+//             connection.destroy();
+//             return interaction.reply('There was an error connecting to the voice channel.');
+//         }
+//     }
+// }
