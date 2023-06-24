7eef9d415352df962bcb1b7f7a453ffdfdbc2a0b
DeLuchxs <liberi1418@gmail.com>
2023-06-24 14:16:16 +0200

    some changes and bot is still quiet in channel

--- commands/fun/join.js
+++ commands/fun/join.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { createReadStream } = require('node:fs');
 
-const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 const path = require('node:path');
 
 module.exports = {
@@ -12,24 +13,24 @@ module.exports = {
         if(!voiceChannel) return interaction.reply('You need to be in a voice channel to use this command.');
         let connection;
         try {
-            const connection = joinVoiceChannel({
+            connection = joinVoiceChannel({
                 channelId: interaction.member.voice.channel.id,
                 guildId: interaction.guild.id,
                 adapterCreator: interaction.guild.voiceAdapterCreator,
             });
             const player = createAudioPlayer();
-            const resource = createAudioResource(path.join(__dirname, 'audio', 'elevator.mp3'), {
-                inlineVolume: true,
+            let resource = createAudioResource(createReadStream(path.join(__dirname, '..', '..', 'audio', 'elevator.ogg')), {
+                inputType: StreamType.OggOpus,
             });
-            player.on('idle', () => {
-                player.play(resource);
-            });
+           
             connection.subscribe(player);
             player.play(resource);
+
             return interaction.reply('Joined the voice channel.');
         } catch (error) {
             if(connection)
             connection.destroy();
+            console.log(error);
             return interaction.reply('There was an error connecting to the voice channel.');
         }
     }
